cfaf1a6ebc534ee6dcaad0edca197247f9ae1463
quylua98 <61273915+quylua98@users.noreply.github.com>
2020-02-24 09:54:52 +0700

    Update WebSecurityConfig.java
    Update config

--- src/main/java/com/travel/config/WebSecurityConfig.java
+++ src/main/java/com/travel/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors()
-        .and()
+        .disable()
         .csrf()
         .disable()
         .exceptionHandling()
